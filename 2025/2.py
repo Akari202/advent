df8e7b21c0abf95469d1e1f60c5d79301a95d99f
Akari202 <akaritwo0two@gmail.com>
2025-12-02 09:29:10 -0500

    day 2, slow

---
+++ 2025/2.py
@@ -0,0 +1,46 @@
+from aoc import puzzle
+
+# puzzle.test_answer = 1227775554
+data = puzzle.csv_lines()[0]
+
+count = 0
+for i in data:
+    split = [int(i) for i in i.split("-")]
+    for j in range(split[0], split[1] + 1):
+        j_str = str(j)
+        j_len = len(j_str)
+        if j_len % 2 != 0:
+            continue
+        mid = j_len // 2
+        # print(f"{j = } | {j_len = } | {mid = } | {j_str[:mid] = } | {j_str[mid:] = }")
+        if j_str[:mid] == j_str[mid:]:
+            count += j
+
+puzzle.submit_one(count)
+
+# puzzle.test_answer = 4174379265
+# data = puzzle.csv_lines()[0]
+
+count = 0
+for i in data:
+    split = [int(i) for i in i.split("-")]
+    for j in range(split[0], split[1] + 1):
+        j_str = str(j)
+        j_len = len(j_str)
+        for k in range(j_len):
+            if k != 0 and j_len % k != 0:
+                continue
+            sub = j_str[:k]
+            comp = ""
+            if k == 0:
+                repeats = j_len
+            else:
+                repeats = j_len // k
+            for _ in range(repeats):
+                comp += sub
+            if comp == j_str:
+                count += j
+                # print(f"{j = } | {j_len = } | {k = } | {sub = } | {comp = }")
+                break
+
+puzzle.submit_two(count)
